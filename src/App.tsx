7b33222410d38caf47fb670214772e208f6164aa
Vivek Verma <97779638+vivekverma52@users.noreply.github.com>
2025-02-07 22:41:01 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,6 @@ function App() {
 
   return (
     <div className="app">
-      <h1 className="center fs-30">React Invoice Generator</h1>
       <InvoicePage data={data} onChange={onInvoiceUpdated} />
     </div>
   )
